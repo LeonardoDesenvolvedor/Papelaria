cec9a2bb8860f950a3eb7f070473edb869527b68
leona <leona@DESKTOP-Q5345HU>
2021-12-06 19:13:53 -0300

    Insercao de Categoria

---
+++ Papelaria/Papelaria/Controllers/CategoriasController.cs
@@ -0,0 +1,50 @@
+﻿using Papelaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Papelaria.Controllers
+{
+    public class CategoriasController : Controller
+    {
+
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome = "Caderno Paltado"
+            },
+
+        };
+
+
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+
+       // Metodo Create()
+        public ActionResult Create(Categoria categoria)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
