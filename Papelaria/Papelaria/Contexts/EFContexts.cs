4f3e7b0a84d6d422b2a4d723481a3ce749567816
leona <leona@DESKTOP-Q5345HU>
2021-12-06 21:28:47 -0300

    Criacao do EFContexts

--- Papelaria/Papelaria/Contexts/EFContexts.cs
+++ Papelaria/Papelaria/Contexts/EFContexts.cs
@@ -11,13 +11,15 @@ namespace Papelaria.Contexts
     {
         public class EFContexts : DbContext
         {
-            public EFContexts() : base("") 
+            public EFContexts() : base("db_Papelaria") 
             {
             }
             
             public  DbSet<Categoria> Categorias { get; set; }
 
             public DbSet<Fornecedor> Fornecedores { get; set; }
+
+            public DbSet<Produto> Produtos { get; set; }
             
         }
     }
